653d2b14ce7b46924c3a19436b3932b983267a99
Ray <ray10102@gmail.com>
2021-01-31 15:41:05 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  fetch('https://api.github.com/orgs/facebook/public_members')
+  .then(response => response.json())
+  .then(data => console.log(data));
+
   return (
     <div className="App">
       <header className="App-header">
